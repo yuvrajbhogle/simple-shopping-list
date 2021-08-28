25512b117c4af1c49f42f00a7a117f9736a20974
yuvrajbhogle <bhogleyuvraj@yahoo.in>
2021-08-28 17:19:54 +0530

    Updates

---
+++ precache-manifest.82016137995b40ba3960f6eb28b8c023.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71daf68a5a0d48e9b2348e418c3b6c76",
+    "url": "/simple-shopping-list/index.html"
+  },
+  {
+    "revision": "69bbd3964c9bf73e5554",
+    "url": "/simple-shopping-list/static/css/main.f9fc5f6d.chunk.css"
+  },
+  {
+    "revision": "179fd825c86e77fe880b",
+    "url": "/simple-shopping-list/static/js/2.0a6be3c7.chunk.js"
+  },
+  {
+    "revision": "38d5cc1cb1001b9cc38b06e2da080c18",
+    "url": "/simple-shopping-list/static/js/2.0a6be3c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69bbd3964c9bf73e5554",
+    "url": "/simple-shopping-list/static/js/main.95ab2a68.chunk.js"
+  },
+  {
+    "revision": "30c351d967e026f3a033",
+    "url": "/simple-shopping-list/static/js/runtime-main.52c35050.js"
+  }
+]);
